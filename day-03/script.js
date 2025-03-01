f4370b66b92ab13a9d30d069977b159406c6d622
Hafsa Egal <hafsaegal915@gmail.com>
2025-03-01 13:57:05 +0300

    work pasted

--- day-03/script.js
+++ day-03/script.js
@@ -1,4 +1,75 @@
-let a=10;
-let b= 20;
-
 console.log(a + b)
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+
+let f_name = "Hafsa";
+let l_name = "Egal";
+
+console.log(f_name + l_name);
+
+let count = 5;
+console.log(count++);
+console.log(count);
+console.log(++count);
+
+//comparison operators
+4 == 5;
+console.log(4 == 5);
+console.log(0 == false);
+console.log(3 == "3");
+
+//strict equality operator
+console.log(null === null);
+console.log(undefined === undefined);
+
+//logical operators
+4 > 5 && 4 == 6;
+console.log(4 > 5 && 4 == 6);
+
+//Tenary operator
+//condition ? val1 :val2
+
+let age = 23;
+age >= 60 ? "senior citizen" : "non senior citizen";
+1;
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+
+let f_name = "Hafsa";
+let l_name = "Egal";
+
+console.log(f_name + l_name);
+
+let count = 5;
+console.log(count++);
+console.log(count);
+console.log(++count);
+
+//comparison operators
+4 == 5;
+console.log(4 == 5);
+console.log(0 == false);
+console.log(3 == "3");
+
+//strict equality operator
+console.log(null === null);
+console.log(undefined === undefined);
+
+//logical operators
+4 > 5 && 4 == 6;
+console.log(4 > 5 && 4 == 6);
+
+//Tenary operator
+//condition ? val1 :val2
+
+let age = 23;
+age >= 60 ? "senior citizen" : "non senior citizen";
